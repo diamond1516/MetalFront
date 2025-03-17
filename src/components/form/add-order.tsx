7797385f95acdb25b4fdb405e6722dc71b8aa3bf
user <ahmadboyabdurahimov589@gmail.com>
2025-03-17 17:23:45 +0500

    new changes

---
+++ src/components/form/add-order.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export default function AddOrderButton() {
+    return (
+        <div>
+            <Button variant="outline" className="text-gray-900">
+                + Buyurtma
+            </Button>
+        </div>
+    )
+}
--- src/components/sidebar/sidebar-provider.tsx
+++ src/components/sidebar/sidebar-provider.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import {
     SidebarInset,
@@ -7,28 +6,27 @@ import {
 } from "@/components/ui/sidebar"
 import { Outlet } from "@tanstack/react-router"
 import { Search } from "lucide-react"
+import AddOrderButton from "../form/add-order"
 import { AppSidebar } from "./app-sidebar"
 
 export default function AppSidebarProvider() {
     return (
         <main className="w-full">
             <aside className="w-full bg-gray-900 text-white h-screen">
-                <SidebarProvider>
+                <SidebarProvider className="bg-ba">
                     <AppSidebar />
                     <SidebarInset>
                         <header className="flex h-16 items-center gap-4 border-b px-6">
                             <SidebarTrigger className="block text-gray-900 dark:text-gray-100" />
-                            <div className="relative flex-1 max-w-md">
+                            <div className="relative ml-auto">
                                 <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
                                 <Input
                                     type="search"
                                     placeholder="Qidirish..."
-                                    className="w-full pl-8"
+                                    className="w-full pl-8 text-gray-900 dark:text-gray-100"
                                 />
                             </div>
-                            <Button className="text-gray-900" variant="outline">
-                                + Buyurtma
-                            </Button>
+                            <AddOrderButton />
                         </header>
                         <div className="p-4">
                             <Outlet />
